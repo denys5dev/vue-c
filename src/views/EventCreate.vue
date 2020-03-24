24e1b9e1af1673070fd5a8ab6f277580e1f84306
denys5dev <denys5dev@gmail.com>
2020-03-24 09:50:02 +0100

    mixin

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,60 +1,120 @@
 <template>
   <div>
-    <div>Create an Event</div>
+    <h1>Create an Event</h1>
     <form @submit.prevent="createEvent">
-      <label>Select a category</label>
-      <select v-model="event.category">
-        <option v-for="cat in categories" :key="cat">{{ cat }}</option>
-      </select>
+      <BaseSelect
+        label="Select a category"
+        :options="categories"
+        v-model="event.category"
+        :class="{ error: $v.event.category.$error }"
+        @blur="$v.event.category.$touch()"
+      />
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">
+          Category is required.
+        </p>
+      </template>
 
       <h3>Name & describe your event</h3>
       <BaseInput
-        type="text"
-        placeholder="Add a title"
-        class="field"
         label="Title"
         v-model="event.title"
-      />
-      <BaseInput
         type="text"
-        placeholder="Add a description"
+        placeholder="Title"
         class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
+      />
+
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">
+          Title is required.
+        </p>
+      </template>
+
+      <BaseInput
         label="Description"
         v-model="event.description"
+        type="text"
+        placeholder="Description"
+        class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">
+          Description is required.
+        </p>
+      </template>
 
       <h3>Where is your event?</h3>
       <BaseInput
-        type="text"
-        placeholder="Add a location"
-        class="field"
         label="Location"
         v-model="event.location"
+        type="text"
+        placeholder="Location"
+        class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"
       />
+
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">
+          Location is required.
+        </p>
+      </template>
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker
+          v-model="event.date"
+          placeholder="Select a date"
+          :input-class="{ error: $v.event.date.$error }"
+          @opened="$v.event.date.$touch()"
+        />
       </div>
 
-      <div class="field">
-        <label>Select a time</label>
-        <select v-model="event.time">
-          <option v-for="time in times" :key="time">{{ time }}</option>
-        </select>
-      </div>
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">
+          Date is required.
+        </p>
+      </template>
 
-      <input type="submit" class="button -fill-gradient" value="Submit" />
+      <BaseSelect
+        label="Select a time"
+        :options="times"
+        v-model="event.time"
+        class="field"
+        :class="{ error: $v.event.time.$error }"
+        @blur="$v.event.time.$touch()"
+      />
+
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">
+          Time is required.
+        </p>
+      </template>
+
+      <BaseButton
+        type="submit"
+        buttonClass="-fill-gradient"
+        :disabled="$v.$anyError"
+        >Submit</BaseButton
+      >
+      <p v-if="$v.$anyError" class="errorMessage">
+        Please fill out the required field(s).
+      </p>
     </form>
   </div>
 </template>
 
 <script>
-// import { mapState, mapGetters } from 'vuex'
-import NProgress from 'nprogress'
 import Datepicker from 'vuejs-datepicker'
+import NProgress from 'nprogress'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   components: {
@@ -71,32 +131,45 @@ export default {
       event: this.createFreshEventObject()
     }
   },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required }
+    }
+  },
   methods: {
     createEvent() {
-      NProgress.start()
-      this.$store
-        .dispatch('event/createEvent', this.event)
-        .then(() => {
-          this.$router.push({
-            name: 'event-show',
-            params: { id: this.event.id }
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        NProgress.start()
+        this.$store
+          .dispatch('event/createEvent', this.event)
+          .then(() => {
+            this.$router.push({
+              name: 'event-show',
+              params: { id: this.event.id }
+            })
+            this.event = this.createFreshEventObject()
           })
-          this.event = this.createFreshEventObject()
-        })
-        .catch(() => {
-          NProgress.dode()
-        })
+          .catch(() => {
+            NProgress.done()
+          })
+      }
     },
     createFreshEventObject() {
       const user = this.$store.state.user.user
-      const id = Math.floor(Math.random() * 100000)
+      const id = Math.floor(Math.random() * 10000000)
 
       return {
         id: id,
         user: user,
         category: '',
         organizer: user,
-        titie: '',
+        title: '',
         description: '',
         location: '',
         date: '',
@@ -105,18 +178,5 @@ export default {
       }
     }
   }
-  // computed: mapState({
-  //   user: 'user',
-  //   categories: 'categories'
-  // })d
-  // computed: {
-  //   ...mapState(['user', 'categories'])
-  // }
 }
 </script>
-
-<style lang="scss" scoped>
-.field {
-  margin-bottom: 24px;
-}
-</style>
